9424520828d9a9ac2238ecfdd37607656000e704
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-04 12:13:33 +0530

    REFACTORED : Validated last name and created seprate function for validatation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,13 +2,24 @@
 
 #User Registration System needs to ensure all validations are in place during the User Entry
 printf '%s\n' "----------------User Registration---------------------------"
-printf "Please enter the first name:"
+
+function validate(){
+   if [[ $1 =~ $2 ]]
+      then
+         echo "valid"
+      else
+         echo "invalid"
+   fi
+}
+
+#VALIDATE FIRST NAME
+printf "Please enter the first name:\n"
 read firstName
 isFirstNameValid="^[A-Z]{1}[a-z]{2,}$"
-if [[ $firstName =~ $isFirstNameValid ]]
-   then
-      printf "first name is valid\n"
-   else
-      printf "first name is invalid\n"
-fi
+printf "your first name is $( validate $firstName $isFirstNameValid )\n"
 
+#VALIDATE LAST NAME
+printf "Please enter the Last name:\n"
+read lastName
+isLastNameValid="^[A-Z]{1}[a-z]{2,}$"
+printf "yout last name is $( validate $lastName $isLastNameValid )\n"
