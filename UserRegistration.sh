b6676d17120084c5a75686198452e65cbbea6464
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-04 16:20:09 +0530

    REFACTORED : function is removed

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,23 +3,28 @@
 #User Registration System needs to ensure all validations are in place during the User Entry
 printf '%s\n' "----------------User Registration---------------------------"
 
-function validate(){
-   if [[ $1 =~ $2 ]]
-      then
-         echo "valid"
-      else
-         echo "invalid"
-   fi
-}
-
 #VALIDATE FIRST NAME
 printf "Please enter the first name:\n"
 read firstName
 isFirstNameValid="^[A-Z]{1}[a-z]{2,}$"
-printf "your first name is $( validate $firstName $isFirstNameValid )\n"
+if [[ $firstName =~ $isFirstNameValid ]]
+   then
+      printf "valid\n"
+   else
+      printf "invalid\n"
+fi
 
 #VALIDATE LAST NAME
-printf "Please enter the Last name:\n"
+printf "Please enter the last name:\n"
 read lastName
 isLastNameValid="^[A-Z]{1}[a-z]{2,}$"
-printf "yout last name is $( validate $lastName $isLastNameValid )\n"
+if [[ $firstName =~ $isFirstNameValid ]]
+   then
+      printf "valid\n"
+   else  
+      printf "invalid\n"
+fi
+
+
+
+
